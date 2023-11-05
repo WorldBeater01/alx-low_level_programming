3971275b60522b089a490745d7641902617858ec
WorldBeater01 <pelumibadiru@gmail.com>
2023-11-05 05:12:31 -0800

    Print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints all arguments
+ * @argc: Argument number
+ * @argv: Argument name
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i=0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
