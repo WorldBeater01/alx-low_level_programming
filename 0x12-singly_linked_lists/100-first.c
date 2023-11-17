ee6050e34bf89362cab9cfe7d8b0bc5f6f52a108
WorldBeater01 <pelumibadiru@gmail.com>
2023-11-17 12:13:11 -0800

    prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void before(void) __attribute__((constructor));
+/**
+ * before - Prints before main
+ * Return: 0
+ */
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n")
+	printf("I bore my house upon my back!\n");
+}
