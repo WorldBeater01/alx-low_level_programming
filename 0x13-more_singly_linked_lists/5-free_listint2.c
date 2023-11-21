85d3a24c052ca1ed2d69b358f40c722042db8e7f
WorldBeater01 <pelumibadiru@gmail.com>
2023-11-21 15:20:42 -0800

    Free spree

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2  - Frees a list
+ * @head: head of the list
+ *
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
